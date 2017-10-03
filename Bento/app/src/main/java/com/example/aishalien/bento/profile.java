e1c8cfcf15cfc94a5d8bc4009eab5771d5495554
tsai41 <kile107107@gmail.com>
2017-10-03 22:08:02 +0800

    個人資料頁面＋修改畫面
    button 修改保存後暫無跳頁，
    標題尚未改成使用者名稱

---
+++ Bento/app/src/main/java/com/example/aishalien/bento/profile.java
@@ -0,0 +1,60 @@
+package com.example.aishalien.bento;
+
+/**
+ * Created by yuting on 2017/10/3.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        Button mModifyButton = (Button) findViewById(R.id.modify_button);
+        mModifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(profile.this,profile_modify.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
---
+++ Bento/app/src/main/java/com/example/aishalien/bento/profile_modify.java
@@ -0,0 +1,19 @@
+package com.example.aishalien.bento;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yuting on 2017/10/3.
+ */
+
+
+public class profile_modify extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_modify);
+
+        //無點擊button跳頁
+    }
+}
