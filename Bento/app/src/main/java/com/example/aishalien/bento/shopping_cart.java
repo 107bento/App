aa9203ec28e9cda049ad28a4b81ae59634ddacf7
AishaLien <simplelienn@gmail.com>
2017-10-07 11:41:57 +0800

    購物車頁面及基本邏輯合併

--- Bento/app/src/main/java/com/example/aishalien/bento/shopping_cart.java
+++ Bento/app/src/main/java/com/example/aishalien/bento/shopping_cart.java
@@ -11,25 +11,34 @@ import java.util.Map;
 import java.util.Objects;
 
 public class shopping_cart extends ListActivity {
-    List<Map<String, String>> mList;
+    List<Map<String,Object>> mList;
+    //Map<String, Object> map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
         mList =  new ArrayList<>();
         String[] listFromResource = getResources().getStringArray(R.array.list_shopping_cart);
+        String[] listFromMeal = getResources().getStringArray(R.array.list_shopping_cart_meal);
+        String[] listFromNum = getResources().getStringArray(R.array.list_shopping_cart_num);
 
+        Map<String, Object> item ;
         for(int i=0; i<listFromResource.length;i++) {
-            Map<String, Objects> item = new HashMap<String, Objects>();
-            item.put("imgView", android.R.drawable.ic_menu_my_calendar);
-            item.put("textView",listFromResource[i]);
+            item = new HashMap<String, Object>();
+            item.put("store_name",listFromResource[i]);
+            item.put("store_pic", R.drawable.meal_1);
+            item.put("meal_name",listFromMeal[i]);
+            item.put("meal_num",listFromNum[i]);
             mList.add(item);
         }
-        SimpleAdapter adapter = new SimpleAdapter(this,mList,R.layout.list_item_pictext,new String[]{"imgView",
-                "txtView","txtView","txtView"},new int[]{R.id.imgView,R.id.stroe_name,R.id.meal_name,R.id.num_beto});
-        setListAdapter(adapter);
-    }
-    public void init_arraylist(){
+        //執行環境 this,帶入資料,layout位置,data 帶入的key,key值帶入哪個元件
+        SimpleAdapter adapter = new SimpleAdapter(
+                this,
+                mList,
+                R.layout.list_item_pictext,
+                new String[]{"store_name", "store_pic","meal_name","meal_num"},
+                new int[]{R.id.store_name,R.id.store_pic,R.id.meal_name,R.id.meal_num});
 
+        setListAdapter(adapter);
     }
 }
