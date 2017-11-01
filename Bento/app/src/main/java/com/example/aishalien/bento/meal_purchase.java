fb04c5bffa9654ea7c2a9183295bc4be627acf7a
AishaLien <simplelienn@gmail.com>
2017-11-01 19:33:04 +0800

    meal_purchase 頁面邏輯 除了價格以外完成

--- Bento/app/src/main/java/com/example/aishalien/bento/meal_purchase.java
+++ Bento/app/src/main/java/com/example/aishalien/bento/meal_purchase.java
@@ -6,13 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class meal_purchase extends AppCompatActivity {
-
+    TextView value,num;
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_purchase);
+        value= (TextView) findViewById(R.id.meal_purchase_value);
+        num= (TextView) findViewById(R.id.meal_purchase_num);
+        ImageView img= (ImageView) findViewById(R.id.meal_purchase_img);
+        Bundle bundle = getIntent().getExtras();
+        value.setText("單價    NT."+bundle.getString("value")+" 元");
+        img.setImageResource(bundle.getInt("pic"));
         initimgbtn();
     }
     public void initimgbtn(){
