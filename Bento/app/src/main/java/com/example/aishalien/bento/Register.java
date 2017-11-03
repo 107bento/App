693a63f61bd6945390a58f8b96df3be93382a729
Unknown <kile107107@gmail.com>
2017-11-03 18:10:54 +0800

    註冊、儲值頁面

---
+++ Bento/app/src/main/java/com/example/aishalien/bento/Register.java
@@ -0,0 +1,20 @@
+package com.example.aishalien.bento;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuting on 2017/11/3.
+ */
+
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
+
